e94dc5fc15b730cfcb8843d6824b63ac2dba032c
yanna <barlisan.al@obmontessori.edu.ph>
2026-04-17 13:20:53 +0800

    update main

---
+++ main.js
@@ -0,0 +1,7 @@
+function passwordChecker (){
+    let password = "";
+    do {
+        password = prompt("Enter a password longer than 4 characters", "");
+    }   while (password.length < 5);
+    window.alert ("Entered Password: ", password);
+}
